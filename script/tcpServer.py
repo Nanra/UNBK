1fa8471e816a3b0ea2310e26bf813c1f0a102124
Nanra <sukedynanra@gmail.com>
2017-12-08 00:58:12 +0700

    Update TCP Server

--- script/tcpServer.py
+++ script/tcpServer.py
@@ -1,6 +1,6 @@
 import socket
 
-def Main():
+def main():
     host = '127.0.0.1'
     port = 5000
 
@@ -9,17 +9,18 @@ def Main():
 
     s.listen(1)
     c, addr = s.accept()
-    print ("Connection from : ") + str(addr)
+    print "Connection from : " + str(addr)
     while True:
         data = c.recv(1024)
         if not data:
             break
-        print "from connected user : " + str(data)
+        print "From connected user : " + str(data)
         data = str(data).upper()
-        print "sending :"+str(data)
+        print "Sending :" + str(data)
         c.send(data)
 
     c.close()
-if __name__ == '__main__':
-    Main()
 
+
+if __name__ == '__main__':
+    main()
