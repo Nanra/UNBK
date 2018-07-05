e38621db4c68b1e415b55c1a9228402347dacac7
Nanra <sukedynanra@gmail.com>
2018-07-05 15:48:39 +0700

    Update Baca Soal

--- script/bacaSoal.py
+++ script/bacaSoal.py
@@ -6,19 +6,19 @@ arrayjawaban = []
 
 
 def main():
-    bacaan_sapaan = open("Sapaan_Umum.txt", "r")
-    bacaan_soal = open("Soal_Pengetahuan_Sosial_IPS.txt", "r")
-    bacaan_jawaban = open("Jawaban_Pengetahuan_Sosial_IPS.txt", "r")
-    parsing_sapaan = bacaan_sapaan.readlines()
+    # bacaan_sapaan = open("Sapaan_Umum.txt", "r")
+    bacaan_soal = open("Soal_Dasar_Bahasa Indonesia.txt", "r")
+    bacaan_jawaban = open("Jawaban_Dasar_Bahasa Indonesia.txt", "r")
+    # parsing_sapaan = bacaan_sapaan.readlines()
     parsing_soal = bacaan_soal.readlines()
     parsing_jawaban = bacaan_jawaban.readlines()
     bacaan_soal.close()
     bacaan_jawaban.close()
 
-    for s in parsing_sapaan:
-        kalimat_sapaan = ('"{}"'.format(s))
-        isi_sapaan = 'google_speech -l id ' + kalimat_sapaan + ' -e speed 1 '
-        perintah.call(isi_sapaan, shell=True)
+    # for s in parsing_sapaan:
+    #    kalimat_sapaan = ('"{}"'.format(s))
+    #    isi_sapaan = 'google_speech -l id ' + kalimat_sapaan + ' -e speed 1 '
+    #    perintah.call(isi_sapaan, shell=True)
 
     i = 0
     for line in parsing_soal:
@@ -31,7 +31,7 @@ def main():
     while i < len(parsing_soal):
         print arraysoal
         kalimat_soal = ('"Soal Nomor {} ...., {}"'.format(i + 1, arraysoal[i]))
-        isi_soal = 'google_speech -l id ' + kalimat_soal + ' -e speed 0.9 '
+        isi_soal = 'google_speech -l id ' + kalimat_soal + ' -e speed 1 '
         print i, isi_soal
         perintah.call(isi_soal, shell=True)
 
